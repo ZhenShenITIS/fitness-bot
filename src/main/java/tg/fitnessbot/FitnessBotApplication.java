6bbc06862f36b88e98037573d4b5e64506269b14
ZhenShen.ITIS <ewgenii20066@gmail.com>
2025-08-01 14:01:40 +0300

    Добавлены новые файлы

---
+++ src/main/java/tg/fitnessbot/FitnessBotApplication.java
@@ -0,0 +1,13 @@
+package tg.fitnessbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tg/fitnessbot/FitnessBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tg.fitnessbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
