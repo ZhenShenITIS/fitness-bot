f092af9f6f45277c168d03c565d47340b499ae7a
ZhenShen.ITIS <ewgenii20066@gmail.com>
2025-08-03 14:04:59 +0300

    Обновлена логика добавления экземпляров класса Command в контейнер команд

--- src/main/java/tg/fitnessbot/command/CommandContainer.java
+++ src/main/java/tg/fitnessbot/command/CommandContainer.java
@@ -2,6 +2,7 @@ package tg.fitnessbot.command;
 
 import com.google.common.collect.ImmutableMap;
 import org.glassfish.jersey.internal.util.collection.ImmutableMultivaluedMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static tg.fitnessbot.constants.CommandName.ADD_FOOD;
@@ -12,10 +13,15 @@ public class CommandContainer {
     private final Command unknownCommand;
     private final ImmutableMap<String, Command> commands;
 
+    @Autowired
+    StartCommand startCommand;
+    @Autowired
+    AddFoodCommand addFoodCommand;
+
     public CommandContainer() {
         commands = ImmutableMap.<String, Command>builder()
-                .put(START.getCommandName(), new StartCommand())
-                .put(ADD_FOOD.getCommandName(), new AddFoodCommand())
+                .put(START.getCommandName(), startCommand)
+                .put(ADD_FOOD.getCommandName(), addFoodCommand)
                 .build();
         unknownCommand = new UnknownCommand();
     }
