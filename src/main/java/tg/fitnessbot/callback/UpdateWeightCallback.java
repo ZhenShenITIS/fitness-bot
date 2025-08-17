d25a8c4824a56e60d949da7f741fe01c88b46a14
ZhenShen.ITIS <ewgenii20066@gmail.com>
2025-08-17 21:03:20 +0400

    Revert "Попробовал реализовать отправку двух сообщений после изменения веса"
    This reverts commit f88cde9e7310f39ce66be762c85fbb3873eb8595.

--- src/main/java/tg/fitnessbot/callback/UpdateWeightCallback.java
+++ src/main/java/tg/fitnessbot/callback/UpdateWeightCallback.java
@@ -2,18 +2,13 @@ package tg.fitnessbot.callback;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.starter.SpringWebhookBot;
-import tg.fitnessbot.command.CommandContainer;
 import tg.fitnessbot.config.TelegramConfig;
 import tg.fitnessbot.constants.CallbackName;
-import tg.fitnessbot.constants.CommandName;
 import tg.fitnessbot.dto.UserForm;
 import tg.fitnessbot.services.UserService;
 
@@ -23,12 +18,6 @@ public class UpdateWeightCallback implements Callback {
 
     @Autowired
     TelegramConfig telegramConfig;
-
-    @Autowired
-    SpringWebhookBot springWebhookBot;
-
-    @Autowired
-    CommandContainer commandContainer;
 
     @Autowired
     UserService userService;
@@ -48,15 +37,7 @@ public class UpdateWeightCallback implements Callback {
         }
         userService.updateUser(user);
         telegramConfig.getUserStateMap().put(user.getId(), CallbackName.NONE);
-        SendMessage msg1 = SendMessage.builder().chatId(message.getChatId()).text("Ваш вес успешно обновлен!").build();
-        SendMessage msg2 = (SendMessage) commandContainer.retrieveCommand(CommandName.START.getCommandName()).handleCommand(message);
-        try {
-            springWebhookBot.execute(msg1);
-            springWebhookBot.execute(msg2);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
-        return null;
+        return SendMessage.builder().chatId(message.getChatId()).text("Ваш вес успешно обновлен!").build();
     }
 
     @Override
