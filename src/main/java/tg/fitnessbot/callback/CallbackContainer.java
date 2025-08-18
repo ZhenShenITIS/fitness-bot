ad8752f8ee0f901abb13a5343dcddb7498ecebce
ZhenShen.ITIS <ewgenii20066@gmail.com>
2025-08-18 16:42:55 +0400

    убрал лишние импорты

--- src/main/java/tg/fitnessbot/callback/CallbackContainer.java
+++ src/main/java/tg/fitnessbot/callback/CallbackContainer.java
@@ -1,15 +1,9 @@
 package tg.fitnessbot.callback;
 
 import com.google.common.collect.ImmutableMap;
-import lombok.Data;
 import org.springframework.stereotype.Component;
-import tg.fitnessbot.command.Command;
-import tg.fitnessbot.command.UnknownCommand;
-import tg.fitnessbot.constants.CallbackName;
 
 import java.util.HashMap;
-
-import static tg.fitnessbot.constants.CallbackName.*;
 
 @Component
 public class CallbackContainer {
